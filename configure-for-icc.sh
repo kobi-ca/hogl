f94e579e1bda394fe9e462edbc07ca37eb13e4a5
Kobi Cohen-Arazi <kobi.cohen-arazi@xcom-tech.com>
2019-03-08 16:53:06 -0800

    configure-for-icc.sh: added --libdir=/usr/lib64
    needed for make rpm

--- configure-for-icc.sh
+++ configure-for-icc.sh
@@ -2,4 +2,4 @@
 
 
 ./bootstrap
-CC=/opt/intel/bin/icc CXX=/opt/intel/bin/icc CXXFLAGS="-Wall -O3 -g -std=c++11" ./configure --prefix=/usr/
+CC=/opt/intel/bin/icc CXX=/opt/intel/bin/icc CXXFLAGS="-Wall -O3 -g -std=c++11" ./configure --prefix=/usr/ --libdir=/usr/lib64
