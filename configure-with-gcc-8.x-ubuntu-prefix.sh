8e7bc0f3488bc3d1f92eadf70decd2f3c96ac87a
Kobi Cohen-Arazi <kobi.cohen-arazi@xcom-labs.com>
2019-08-15 17:14:53 -0700

    configure-with-gcc-8.x-ubuntu-prefix.sh change libdir
    libdir for ubuntu is /usr/lib

--- configure-with-gcc-8.x-ubuntu-prefix.sh
+++ configure-with-gcc-8.x-ubuntu-prefix.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# used with Ubuntu docker for maxwell
+
 ./bootstrap
- CC="gcc-8" CXX="g++-8" CXXFLAGS="-Wall -O3 -g -std=c++11" ./configure CC=/opt/rh/devtoolset-7/root/usr/bin/gcc CXX=/opt/rh/devtoolset-7/root/usr/bin/g++ --prefix=/usr --libdir=/usr/lib64
+ CC="gcc-8" CXX="g++-8" CXXFLAGS="-Wall -O3 -g -std=c++11" ./configure CC=/opt/rh/devtoolset-7/root/usr/bin/gcc CXX=/opt/rh/devtoolset-7/root/usr/bin/g++ --prefix=/usr --libdir=/usr/lib
 
 # for make deb
 # CC="gcc-8" CXX="g++-8" make deb
